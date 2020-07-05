61d1193806b5dca76d38bda678f49e30b7b954ff
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2020-07-05 17:33:28 +0300

    array of products of all values except self without division

--- src/com/sbardyuk/algofun/ArrayProductsExceptSelf.java
+++ src/com/sbardyuk/algofun/ArrayProductsExceptSelf.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 public class ArrayProductsExceptSelf {
 
-    private static int[] findProduct(int[] arr) {
+    private static int[] findProductMath(int[] arr) {
         int all = 1;
         int count0 = 0; // count of 0 values in array
         // if count > 1 all the result values are 0
@@ -38,8 +38,47 @@ public class ArrayProductsExceptSelf {
         return arr;
     }
 
+
+    private static int[] findProduct(int[] arr) {
+        int n = arr.length;
+        int i, temp = 1;
+
+        int[] result = new int [n];
+
+        // Product of elements on left side excluding arr[i]
+        for (i = 0; i < n; i++) {
+            result[i] = temp;
+            temp *= arr[i];
+        }
+
+        // Initializing temp to 1 for product on right side
+        temp = 1;
+
+        // Product of elements on right side excluding arr[i]
+        for (i = n - 1; i >= 0; i--) {
+            result[i] *= temp;
+            temp *= arr[i];
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] res = findProduct(new int[] {1, 2, 3, 4});
+        int[] res = findProductMath(new int[] {1, 2, 3, 4});
+        System.out.println(Arrays.toString(res));
+
+        res = findProductMath(new int[] {4, 2, 1, 5, 0});
+        System.out.println(Arrays.toString(res));
+
+        res = findProductMath(new int[] {4, 2, 0, 5, 0});
+        System.out.println(Arrays.toString(res));
+
+        res = findProductMath(new int[] {-1, 2, -3, 4, -5});
+        System.out.println(Arrays.toString(res));
+
+
+        // without division
+        res = findProduct(new int[] {1, 2, 3, 4});
         System.out.println(Arrays.toString(res));
 
         res = findProduct(new int[] {4, 2, 1, 5, 0});
