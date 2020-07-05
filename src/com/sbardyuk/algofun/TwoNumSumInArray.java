67742b6b8fabfafbdbed8d9eacb43fcd9eec4846
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2020-07-05 14:37:57 +0300

    two number sum in array using map, and using map with one pass

--- src/com/sbardyuk/algofun/TwoNumSumInArray.java
+++ src/com/sbardyuk/algofun/TwoNumSumInArray.java
@@ -1,6 +1,8 @@
 package com.sbardyuk.algofun;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoNumSumInArray {
 
@@ -25,6 +27,41 @@ public class TwoNumSumInArray {
         return arr;
     }
 
+    private static int[] findSumWithMap(int[] arr, int n) {
+        Map<Integer, Integer> complementaryMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            complementaryMap.put(arr[i], i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = n - arr[i];
+
+            if (complementaryMap.containsKey(complement) && complementaryMap.get(complement) != i) {
+                return new int[] {arr[i], arr[complementaryMap.get(complement)]};
+            }
+        }
+
+        return arr;
+    }
+
+    private static int[] findSumWithMapOnePass(int[] arr, int n) {
+        Map<Integer, Integer> complementaryMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = n - arr[i];
+
+            if (complementaryMap.containsKey(complement) && complementaryMap.get(complement) != i) {
+                return new int[] {arr[i], arr[complementaryMap.get(complement)]};
+            }
+
+            complementaryMap.put(arr[i], i);
+        }
+
+        return arr;
+    }
+
+
     public static void main(String[] args) {
         int[] res = findSum(new int[] {2, 4, 5, 7, 8}, 9);
         System.out.println(Arrays.toString(res));
@@ -33,6 +70,26 @@ public class TwoNumSumInArray {
         System.out.println(Arrays.toString(res));
 
         res = findSum(new int[] {2, 4, 5, 7, 8}, 6);
+        System.out.println(Arrays.toString(res));
+
+        // with map
+        res = findSumWithMap(new int[] {2, 4, 5, 7, 8}, 9);
+        System.out.println(Arrays.toString(res));
+
+        res = findSumWithMap(new int[] {2, 4, 5, 7, 8}, 19);
+        System.out.println(Arrays.toString(res));
+
+        res = findSumWithMap(new int[] {2, 4, 5, 7, 8}, 6);
+        System.out.println(Arrays.toString(res));
+
+        // with map one pass only
+        res = findSumWithMapOnePass(new int[] {2, 4, 5, 7, 8}, 9);
+        System.out.println(Arrays.toString(res));
+
+        res = findSumWithMapOnePass(new int[] {2, 4, 5, 7, 8}, 19);
+        System.out.println(Arrays.toString(res));
+
+        res = findSumWithMapOnePass(new int[] {2, 4, 5, 7, 8}, 6);
         System.out.println(Arrays.toString(res));
     }
 
