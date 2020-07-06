31237d1e78b3199af63f4ec8e074c17eacef33ea
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2020-07-06 11:12:00 +0300

    find first unique value in array

---
+++ src/com/sbardyuk/algofun/UniqueInArray.java
@@ -0,0 +1,39 @@
+package com.sbardyuk.algofun;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UniqueInArray {
+
+    private static int findFirstUnique(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int x : arr) {
+            if (countMap.containsKey(x)) {
+                int newCount = countMap.get(x) + 1;
+                countMap.put(x, newCount);
+            } else {
+                countMap.put(x, 1);
+            }
+        }
+
+        for (int x : arr) {
+            if (countMap.get(x) == 1) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int res = findFirstUnique(new int[] {9, 2, 3, 2, 6, 6});
+        System.out.println(res);
+
+        res = findFirstUnique(new int[] {4, 5, 1, 2, 0, 4});
+        System.out.println(res);
+
+        res = findFirstUnique(new int[] {1, 1, 2, 2, 3, 3});
+        System.out.println(res);
+    }
+}
