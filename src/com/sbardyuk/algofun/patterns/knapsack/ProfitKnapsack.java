ebb6bd13b5dae27efc182fb988a397400d0439c2
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2020-12-18 19:47:21 +0200

    knapsack with memoization

--- src/com/sbardyuk/algofun/patterns/knapsack/ProfitKnapsack.java
+++ src/com/sbardyuk/algofun/patterns/knapsack/ProfitKnapsack.java
@@ -1,8 +1,11 @@
 package com.sbardyuk.algofun.patterns.knapsack;
 
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProfitKnapsack {
 
@@ -21,6 +24,44 @@ public class ProfitKnapsack {
         return Math.max(profit1, profit2);
 
     }
+
+    static int knapWithMemo(int [] weights, int [] profits, int capacity) {
+        Map<String, Integer> memo = new HashMap<>();
+        int res = knapmemo(memo, weights, profits, capacity, 0);
+        System.out.println(memo);
+        return res;
+    }
+
+    static int knapmemo(Map<String, Integer> memo, int [] weights, int [] profits, int capacity, int index) {
+        if (index >= weights.length || capacity <= 0) {
+            return 0;
+        }
+
+        int profit1 = 0;
+        if (memo.containsKey(capacity + ":" + index + 1)) {
+            profit1 = memo.get(capacity + ":" + index + 1);
+        } else {
+            profit1 = knap(weights, profits, capacity, index + 1);
+            memo.put(capacity + ":" + (index + 1), profit1);
+        }
+
+        int profit2 = 0;
+        if (weights[index] <= capacity) {
+
+            int tmpProf = 0;
+            if (memo.containsKey(capacity - weights[index] + ":" + (index + 1))) {
+                tmpProf = memo.get(capacity - weights[index] + ":" + (index + 1));
+            } else {
+                tmpProf = knap(weights, profits, capacity - weights[index], index + 1);
+                memo.put(capacity - weights[index] + ":" + (index + 1), profit1);
+            }
+
+            profit2 = profits[index] + tmpProf;
+        }
+
+        return Math.max(profit1, profit2);
+    }
+
 
     static class KnapRes {
         List<Integer> items;
@@ -75,6 +116,9 @@ public class ProfitKnapsack {
 
         System.out.println((knap(new int [] {2, 3, 1, 4}, new int [] {4, 5, 3, 7}, 5, 0)));
         System.out.println((knap(new int [] {1, 6, 10, 16}, new int [] {1, 2, 3, 5}, 6, 0)));
+
+        System.out.println((knapWithMemo(new int [] {2, 3, 1, 4}, new int [] {4, 5, 3, 7}, 5)));
+        System.out.println((knapWithMemo(new int [] {1, 6, 10, 16}, new int [] {1, 2, 3, 5}, 6)));
 
         System.out.println((knapItems(new ArrayList<>(), new int [] {2, 3, 1, 4}, new int [] {4, 5, 3, 7}, 5, 0)));
         System.out.println((knapItems(new ArrayList<>(), new int [] {1, 6, 10, 16}, new int [] {1, 2, 3, 5}, 6, 0)));
