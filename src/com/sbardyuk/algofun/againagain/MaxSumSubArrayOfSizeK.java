2935d49b4ca34ea1adf7a86ec65062258a9ace6c
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2021-06-28 16:03:47 +0300

    Permutation in a String

--- src/com/sbardyuk/algofun/againagain/MaxSumSubArrayOfSizeK.java
+++ src/com/sbardyuk/algofun/againagain/MaxSumSubArrayOfSizeK.java
@@ -2,6 +2,22 @@ package com.sbardyuk.algofun.againagain;
 
 public class MaxSumSubArrayOfSizeK {
 
+    /**
+     * Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+     *
+     * Example 1:
+     *
+     * Input: [2, 1, 5, 1, 3, 2], k=3
+     * Output: 9
+     * Explanation: Subarray with maximum sum is [5, 1, 3].
+     * Example 2:
+     *
+     * Input: [2, 3, 4, 1, 5], k=2
+     * Output: 7
+     * Explanation: Subarray with maximum sum is [3, 4].
+     *
+     */
+
     public static int findMaxSumSubArray(int k, int[] arr) {
 
         int maxSum = 0;
---
+++ src/com/sbardyuk/algofun/againagain/StringContainsAnyPermut.java
@@ -0,0 +1,104 @@
+package com.sbardyuk.algofun.againagain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringContainsAnyPermut {
+    /**
+     * Given a string and a pattern, find out if the string contains any permutation of the pattern.
+     *
+     * Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
+     *
+     * abc
+     * acb
+     * bac
+     * bca
+     * cab
+     * cba
+     * If a string has ‘n’ distinct characters it will have n!n! permutations.
+     *
+     * Example 1:
+     *
+     * Input: String="oidbcaf", Pattern="abc"
+     * Output: true
+     * Explanation: The string contains "bca" which is a permutation of the given pattern.
+     * Example 2:
+     *
+     * Input: String="odicf", Pattern="dc"
+     * Output: false
+     * Explanation: No permutation of the pattern is present in the given string as a substring.
+     * Example 3:
+     *
+     * Input: String="bcdxabcdy", Pattern="bcdyabcdx"
+     * Output: true
+     * Explanation: Both the string and the pattern are a permutation of each other.
+     * Example 4:
+     *
+     * Input: String="aaacb", Pattern="abc"
+     * Output: true
+     * Explanation: The string contains "acb" which is a permutation of the given pattern.
+     */
+
+    public static boolean findPermutation(String str, String pattern) {
+        Map<Character, Integer> freq = freq(pattern);
+        Map<Character, Integer> workfreq = new HashMap<>(freq);
+
+        int windowStart = 0;
+        int windowEnd = 0;
+
+        for (; windowEnd < pattern.length(); windowEnd ++) {
+            decrementAndRemove(workfreq, str.charAt(windowEnd));
+        }
+
+        if (workfreq.isEmpty()) {
+            return true;
+        }
+
+        while (windowEnd < str.length()) {
+            decrementAndRemove(workfreq, str.charAt(windowEnd));
+            windowEnd++;
+
+            incrementAndRemove(workfreq, str.charAt(windowStart));
+            windowStart++;
+
+            if (workfreq.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void incrementAndRemove(Map<Character, Integer> map, Character ch) {
+        addAndRemove(map, ch, +1);
+    }
+
+    private static void decrementAndRemove(Map<Character, Integer> map, Character ch) {
+        addAndRemove(map, ch, -1);
+    }
+
+    private static void addAndRemove(Map<Character, Integer> map, Character ch, int whatToAdd) {
+        int count = map.getOrDefault(ch, 0) + whatToAdd;
+        if (count == 0) {
+            map.remove(ch);
+        } else {
+            map.put(ch, count);
+        }
+    }
+
+    private static Map<Character, Integer> freq (String pattern) {
+        Map<Character, Integer> f = new HashMap<>();
+
+        for (char ch : pattern.toCharArray()) {
+            f.put(ch, f.getOrDefault(ch, 0) + 1);
+        }
+
+        return f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Permutation exist: " + findPermutation("oidbcaf", "abc"));
+        System.out.println("Permutation exist: " + findPermutation("odicf", "dc"));
+        System.out.println("Permutation exist: " + findPermutation("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: " + findPermutation("aaacb", "abc"));
+    }
+}
