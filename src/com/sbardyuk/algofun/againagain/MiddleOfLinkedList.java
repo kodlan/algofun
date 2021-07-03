2ea153b6cc0135fc9ef2b576d95db858f514d46f
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2021-07-03 17:46:00 +0300

    middle of the linked list

---
+++ src/com/sbardyuk/algofun/againagain/MiddleOfLinkedList.java
@@ -0,0 +1,60 @@
+package com.sbardyuk.algofun.againagain;
+
+/**
+ * Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+ *
+ * If the total number of nodes in the LinkedList is even, return the second middle node.
+ *
+ * Example 1:
+ *
+ * Input: 1 -> 2 -> 3 -> 4 -> 5 -> null
+ * Output: 3
+ * Example 2:
+ *
+ * Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+ * Output: 4
+ * Example 3:
+ *
+ * Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
+ * Output: 4
+ *
+ */
+public class MiddleOfLinkedList {
+
+    static class ListNode {
+        int value = 0;
+        ListNode next;
+
+        ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+    }
+
+}
